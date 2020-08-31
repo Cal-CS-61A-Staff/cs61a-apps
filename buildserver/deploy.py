850528193b9adacbd4cde0e56a7162dfaf015969
Rahul Arya <rahularya@berkeley.edu>
2020-08-31 09:35:39 -0700

    Fix timeouts for slow flask jobs (#21)

--- buildserver/deploy.py
+++ buildserver/deploy.py
@@ -164,6 +164,7 @@ def run_dockerfile_deploy(app: App, pr_number: int):
         for job in app.config["tasks"]:
             sh(
                 "gcloud",
+                "beta",
                 "scheduler",
                 "jobs",
                 "create",
@@ -171,6 +172,7 @@ def run_dockerfile_deploy(app: App, pr_number: int):
                 f"{app}-{job['name']}",
                 f"--schedule=\"{job['schedule']}\"",
                 f"--uri=https://{app}.cs61a.org/jobs/{job['name']}",
+                "--attempt-deadline=1200s",
                 "-q",
             )
 
