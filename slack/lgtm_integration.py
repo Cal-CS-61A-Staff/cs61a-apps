49efbbf23ea294b8edb7447d72f3cc6185bc4282
Rahul Arya <rahularya@berkeley.edu>
2020-09-08 04:14:36 -0700

    LGTM Bot (#34)

---
+++ slack/lgtm_integration.py
@@ -0,0 +1,92 @@
+import re
+
+import requests
+from github import Github
+
+from common.rpc.secrets import get_secret
+from integration import Integration
+
+VALID_PATH = r"[0-9A-Za-z\-]+"
+REPO_REGEX = rf"(?P<repo>{VALID_PATH}/{VALID_PATH})"
+PATH_REGEX = rf"(?P<path>{VALID_PATH})"
+
+REGEX_TEMPLATE = (
+    fr"(https?://)?github\.com/{REPO_REGEX}/pull/{PATH_REGEX}/?(\|[^\s|]+)?"
+)
+
+COURSE_ORGS = {"Cal-CS-61A-Staff": "cs61a"}
+
+TRIGGER_WORDS = {
+    "lgtm": "APPROVE",
+    "sucks": "REQUEST_CHANGES",
+    "needs changes": "REQUEST_CHANGES",
+}
+
+
+class LGTMIntegration(Integration):
+    reply = ""
+
+    def process(self):
+        for trigger_word, event in TRIGGER_WORDS.items():
+            if trigger_word in self._message.lower():
+                break
+        else:
+            return
+
+        match = re.search(REGEX_TEMPLATE, self._message)
+
+        if not match:
+            return
+
+        try:
+            repo = match.group("repo")
+            pr = int(match.group("path"))
+        except ValueError:
+            return
+
+        if COURSE_ORGS[repo.split("/")[0]] != self._course:
+            return
+
+        users = requests.get(
+            "https://slack.com/api/users.list", params={"token": self._bot_token}
+        ).json()
+
+        g = Github(get_secret(secret_name="GITHUB_ACCESS_TOKEN"))
+        repo = g.get_repo(repo)
+        pr = repo.get_pull(pr)
+
+        github_email = pr.user.email
+
+        for member in users["members"]:
+            if member["profile"].get("email") == github_email:
+                break
+        else:
+            return
+
+        for member in users["members"]:
+            if member["id"] == self._event["user"]:
+                sender_email = member["profile"].get("email")
+                break
+        else:
+            return
+
+        if sender_email == github_email:
+            return
+
+        pr.create_review(
+            body="{} approved this PR via the Slackbot!".format(
+                member["profile"].get("real_name_normalized")
+            ),
+            event=event,
+        )
+
+        if trigger_word == "lgtm":
+            self.reply = ":white_check_mark: Automatically approved on GitHub!"
+        elif trigger_word == "sucks":
+            self.reply = ":poop: Wow, this really needs changes..."
+        else:
+            self.reply = ":x: Changes requested on GitHub!"
+
+    @property
+    def message(self):
+        return self._message + "\n\n" + self.reply
--- slack/slack_client.py
+++ slack/slack_client.py
@@ -22,6 +22,7 @@ from emoji_integration import EmojiIntegration
 from golink_integration import GoLinkIntegration
 from group_integration import GroupIntegration
 from integration import combine_integrations
+from lgtm_integration import LGTMIntegration
 from piazza_integration import PiazzaIntegration
 from prlink_integration import PRLinkIntegration
 from issue_integration import IssueIntegration
@@ -164,9 +165,11 @@ def create_slack_client(app):
                 integrations.append(PRLinkIntegration)
             if "issues" in active_services:
                 integrations.append(IssueIntegration)
+            if "lgtm" in active_services:
+                integrations.append(LGTMIntegration)
 
             combined_integration = combine_integrations(integrations)(
-                event["text"], token if token is not UNABLE else None, team_id
+                event["text"], token if token is not UNABLE else None, team_id, event
             )
 
             if combined_integration.responses:
