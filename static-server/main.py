af0564dd9524de95d285874e3d19f6f053cdad94
rahularya <rahularya@berkeley.edu>
2020-11-07 07:38:39 -0800

    fix: even more issues with static-server

--- static-server/main.py
+++ static-server/main.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from flask import Flask, abort, current_app, safe_join, send_file
+from flask import Flask, abort, current_app, redirect, safe_join, send_file
 from flask_compress import Compress
 from google.cloud import storage
 import tempfile
@@ -33,17 +33,19 @@ def get_bucket(app_lookup: Dict[str, str]):
 
 
 @app.route("/", defaults={"path": ""})
-@app.route("/<path:path>/", methods=["GET"])
+@app.route("/<path:path>", methods=["GET"])
 def get(path):
     filename = safe_join("/", path)[1:]
+    client = storage.Client()
+    bucket = client.get_bucket(
+        get_bucket({"static-server": "website", "website": "website"})
+    )
     try:
         if not filename:
             raise NotFound(filename)
-        client = storage.Client()
-        bucket = client.get_bucket(
-            get_bucket({"static-server": "website", "website": "website"})
-        )
         blob = bucket.blob(filename)
+        if blob.exists() and path != filename:
+            return redirect("/" + filename)
         with tempfile.NamedTemporaryFile() as temp:
             blob.download_to_filename(temp.name)
             return send_file(temp.name, attachment_filename=filename)
@@ -51,9 +53,12 @@ def get(path):
         if filename.endswith("index.html"):
             abort(404)
         else:
-            if filename and not filename.endswith("/"):
-                filename += "/"
-            return get(filename + "index.html")
+            if path and not path.endswith("/"):
+                if bucket.blob(filename + "/" + "index.html").exists():
+                    return redirect("/" + filename + "/", 301)
+                else:
+                    abort(404)
+            return get(path + "index.html")
 
 
 Compress(app)
