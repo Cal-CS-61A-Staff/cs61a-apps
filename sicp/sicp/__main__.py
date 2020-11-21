b5167666e557e2e35bfa8466177750c31ec180d8
Vanshaj Singhania <svanshaj2001@gmail.com>
2020-11-21 13:35:11 -0800

    Add sicp clone apps

--- sicp/sicp/__main__.py
+++ sicp/sicp/__main__.py
@@ -1,5 +1,5 @@
 import click
-
+from clone import run_apps_clone
 
 @click.group()
 def cli():
@@ -7,6 +7,21 @@ def cli():
     This is an experimental general-purpose 61A task runner.
     """
     pass
+
+@cli.command()
+@click.argument('repo')
+@click.argument('dest', default='')
+def clone(repo, dest):
+    """Clone REPO to DEST.
+
+    REPO is the name of the 61a repo to set up.
+    Currently, only "apps" is supported. By
+    default, DEST is set to the name of REPO.
+    """
+    if repo == "apps":
+        run_apps_clone(dest if dest else 'cs61a-apps')
+    else:
+        click.echo("No need to use sicp for that! Just git clone.", err=True)
 
 
 if __name__ == "__main__":
---
+++ sicp/sicp/clone.py
@@ -0,0 +1,20 @@
+from common.shell_utils import sh
+import os
+
+def run_apps_clone(dir):
+    print("========== Cloning cs61a-apps ==========")
+    sh("git", "clone", "https://github.com/Cal-CS-61A-Staff/cs61a-apps", dir)
+
+    print("====== Installing Black & Prettier =====")
+    if 'black' not in sh("pip3", "list", quiet=True).decode('utf-8'):
+        sh("pip3", "install", "black")
+    if 'prettier' not in sh("npm", "list", "-g", quiet=True).decode('utf-8'):
+        sh("npm", "install", "-g", "prettier")
+
+    print("========== Linking .githooks ===========")
+    os.chdir(dir)
+    sh("chmod", "+x", ".githooks/pre-commit")
+    sh("git", "config", "core.hooksPath", ".githooks")
+
+    print("================ Done! =================")
+
