642c8446e0ad3bee3783acfa29b20d5df8cd9f44
Vanshaj Singhania <8797467+itsvs@users.noreply.github.com>
2020-11-22 09:32:57 -0800

    Add sicp clone apps (#102)
    * Add sicp clone apps
    
    * Resolve dependencies
    
    * Include sicp folder in package
    
    * Include common in package
    
    * Actually include common in package
    
    * Bump up required Python version
    
    * Add sicp clone cs61a
    
    * Update clone help text
    
    * Run black

--- sicp/sicp/__main__.py
+++ sicp/sicp/__main__.py
@@ -1,4 +1,5 @@
 import click
+from sicp.clone import run_apps_clone, run_61a_clone
 
 
 @click.group()
@@ -7,6 +8,24 @@ def cli():
     This is an experimental general-purpose 61A task runner.
     """
     pass
+
+
+@cli.command()
+@click.argument("repo")
+@click.argument("dest", default="")
+def clone(repo, dest):
+    """Clone REPO to DEST.
+
+    REPO is the name of the 61a repo to set up.
+    Currently, "apps" and "cs61a" are supported.
+    By default, DEST is set to the name of REPO.
+    """
+    if repo == "apps":
+        run_apps_clone(dest if dest else "cs61a-apps")
+    elif repo == "cs61a":
+        run_61a_clone(dest if dest else "berkeley-cs61a")
+    else:
+        click.echo("No need to use sicp for that! Just git clone.", err=True)
 
 
 if __name__ == "__main__":
