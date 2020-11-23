23848d56d0f679ab549ec4b52bb0ff846987a966
Vanshaj Singhania <svanshaj2001@gmail.com>
2020-11-23 00:11:06 -0800

    Add content generation script

--- wiki-base/make_content.py
+++ wiki-base/make_content.py
@@ -1,7 +1,30 @@
 import os
 from shutil import copyfile
 
+
+class WikiApp:
+    def __init__(self, path, name=None, dest=None):
+        self.path = path
+        with open(f"../{self.path}/README.md") as r:
+            self.lines = [l for l in r.readlines()]
+
+        self.name = name if name else self.lines[0][2:-1]
+        self.dest = dest if dest else f"{self.path}/_index.md"
+
+
 EXCLUDE = set(["node_modules", ".pytest_cache", "env"])
+
+PATHS = {
+    "examtool": WikiApp("examtool", "Examtool"),
+    "exam-write": WikiApp("exam-write", "Writing Exams", dest="examtool/writing.md"),
+    "oh": WikiApp("oh", "Office Hours Queue"),
+    "oh/migrations": WikiApp(
+        "oh/migrations", "Generating Migrations", dest="oh/migrations.md"
+    ),
+    "sicp": WikiApp("sicp", "SICP"),
+    "piazzaoncall": WikiApp("piazzaoncall", "Piazza Oncall"),
+    "pr_proxy": WikiApp("pr_proxy", "PR Proxy"),
+}
 
 if not os.path.exists("content"):
     os.makedirs("content")
@@ -15,24 +38,16 @@ for root, dirs, files in os.walk("../"):
         if file == "README.md":
             apps.append(root[3:])
 
-for app in apps:
-    print(app)
-    with open(f"../{app}/README.md") as r:
-        lines = [l for l in r.readlines()]
-        name, brief = lines[0][2:-1], lines[1][:-1]
+for app_raw in apps:
+    app = PATHS.get(app_raw, WikiApp(app_raw))
+    print(app_raw)
 
-    if not os.path.exists(f"content/{app}"):
-        os.makedirs(f"content/{app}")
+    if not os.path.exists(f"content/{app.dest.split('/')[0]}"):
+        os.makedirs(f"content/{app.dest.split('/')[0]}")
 
-    with open(f"content/{app}/_index.md", "w") as i:
-        i.write("---\n")
-        i.write(f"title: {name}\n")
-        i.write("---\n\n")
-        i.write(f"{brief}\n")
-
-    with open(f"content/{app}/readme.md", "w") as c:
+    with open(f"content/{app.dest}", "w") as c:
         c.write("---\n")
-        c.write(f"title: README\n")
+        c.write(f"title: {app.name}\n")
         c.write("---\n\n")
-        for l in lines[2:]:
+        for l in app.lines[1:]:
             c.write(f"{l}")
