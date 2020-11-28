493aba2caf4e0952f5ae5399c3927bdc5ea636b6
rahularya <rahularya@berkeley.edu>
2020-11-27 17:04:40 -0800

    fix: record service deployments

--- buildserver/external_repo_utils.py
+++ buildserver/external_repo_utils.py
@@ -1,10 +1,18 @@
-from app_config import App
+from app_config import App, WEB_DEPLOY_TYPES
 from common.db import connect_db
 
 
 def update_config(app: App, pr_number: int):
-    if pr_number == 0:
-        with connect_db() as db:
+    with connect_db() as db:
+        db(
+            "DELETE FROM services WHERE app=(%s) AND pr_number=(%s)",
+            [app.name, pr_number],
+        )
+        db(
+            "INSERT INTO services VALUES (%s, %s, %s, %s)",
+            [app.name, pr_number, False, app.config["deploy_type"] in WEB_DEPLOY_TYPES],
+        )
+        if pr_number == 0:
             db("DELETE FROM apps WHERE app=%s", [app.name])
             if app.config["repo"]:
                 db(
