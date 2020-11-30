6e5298fb66fcfdd3c88cc33a90668490c0c0a48c
Vanshaj Singhania <8797467+itsvs@users.noreply.github.com>
2020-11-29 20:14:22 -0800

    Infrastructure Wiki (again) (#108)
    * Start working on wiki
    
    * Remove .htaccess from Dockerfile
    
    * Create symlinks
    
    * Fix wiki build structure
    
    * Change build_type to hugo
    
    * Remove wiki from this PR
    
    * Move theme elsewhere
    
    * Add apps and convert config to YAML
    
    * Only keep apps with READMEs
    
    * Truncate content
    
    * Add content generation script
    
    * Move index to content script
    
    * Adding content for testing
    
    * Automate content again
    
    * Ignore output files
    
    * Temporarily ignore Cats
    
    * Fix cats README formatting
    
    Co-authored-by: Rahul Arya <rahularya@berkeley.edu>

---
+++ wiki-base/make_content.py
@@ -0,0 +1,57 @@
+import os
+from shutil import copyfile
+
+
+class WikiApp:
+    def __init__(self, path, name=None, dest=None):
+        self.path = path
+        with open(f"../{self.path}/README.md") as r:
+            self.lines = [l for l in r.readlines()]
+
+        self.name = name if name else self.lines[0][2:-1]
+        self.dest = dest if dest else f"{self.path}/_index.md"
+
+
+EXCLUDE = set(["node_modules", ".pytest_cache", "env"])
+
+PATHS = {
+    "examtool": WikiApp("examtool", "Examtool"),
+    "exam-write": WikiApp("exam-write", "Writing Exams", dest="examtool/writing.md"),
+    "oh": WikiApp("oh", "Office Hours Queue"),
+    "oh/migrations": WikiApp(
+        "oh/migrations", "Generating Migrations", dest="oh/migrations.md"
+    ),
+    "sicp": WikiApp("sicp", "SICP"),
+    "piazzaoncall": WikiApp("piazzaoncall", "Piazza Oncall"),
+    "pr_proxy": WikiApp("pr_proxy", "PR Proxy"),
+}
+
+if not os.path.exists("content"):
+    os.makedirs("content")
+
+with open("content/_index.md", "w") as index:
+    index.write("\n# CS 61A Infra Wiki\n")
+    index.write("Welcome to the CS 61A Infrastructure Wiki!\n")
+
+apps = []
+for root, dirs, files in os.walk("../"):
+    dirs[:] = [d for d in dirs if d not in EXCLUDE]
+    if root == "../wiki-base":
+        dirs[:] = []
+    for file in files:
+        if file == "README.md":
+            apps.append(root[3:])
+
+for app_raw in apps:
+    app = PATHS.get(app_raw, WikiApp(app_raw))
+    print(app_raw)
+
+    if not os.path.exists(f"content/{app.dest.split('/')[0]}"):
+        os.makedirs(f"content/{app.dest.split('/')[0]}")
+
+    with open(f"content/{app.dest}", "w") as c:
+        c.write("---\n")
+        c.write(f"title: {app.name}\n")
+        c.write("---\n\n")
+        for l in app.lines[1:]:
+            c.write(f"{l}")
