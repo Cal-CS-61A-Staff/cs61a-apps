de7e6c9a404835a68ccde4692f9767e6c27a1cf5
rahularya <rahularya@berkeley.edu>
2020-11-30 22:48:33 -0800

    Fast latex builds

--- sandbox/main.py
+++ sandbox/main.py
@@ -39,9 +39,7 @@ WORKING_DIRECTORY = abspath("tmp") if app.debug else "/save"
 HOT_RELOAD_SCRIPT_PATH = abspath("hot_reloader.js")
 REPO = "Cal-CS-61A-Staff/berkeley-cs61a"
 
-ENV = dict(
-    _CLOUD_STORAGE_BUCKET="website-base.buckets.cs61a.org"
-)  # fixme: repo needs to install cloud storage in makefile
+ENV = dict(CLOUD_STORAGE_BUCKET="website-base.buckets.cs61a.org")
 
 DEFAULT_USER = "rahularya"
 
