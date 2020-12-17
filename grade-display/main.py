ea1a0a37dac1801b0837169b3b5fa6ac6a80d68e
Vanshaj Singhania <svanshaj2001@gmail.com>
2020-12-16 19:27:48 -0800

    Add calculations, return CSV

--- grade-display/main.py
+++ grade-display/main.py
@@ -1,6 +1,6 @@
 import sys
 
-from flask import Flask, abort, request, redirect
+from flask import Flask, abort, request, redirect, make_response
 from typing import List, Tuple
 
 from common.oauth_client import create_oauth_client, is_staff
@@ -11,6 +11,9 @@ from common.rpc.howamidoing import download_grades
 
 from auth import authenticate, update_storage
 from datetime import datetime
+
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 create_oauth_client(app, "grade-display-exports", return_response=update_storage)
@@ -154,7 +157,67 @@ def delete_acadh():
 def export_grades():
     if not is_staff("cs61a"):
         return redirect(url_for("config"))
-    return download_grades()
+    grades_json = download_grades()
+    df = pd.DataFrame(grades_json["scores"], columns=grades_json["header"])
+
+    df["Midterm 1"] = df["Midterm 1 (Raw)"] + df["Midterm 1 (Recovery)"]
+    df["Midterm 2"] = df["Midterm 2 (Raw)"] + df["Midterm 2 (Recovery)"]
+    df["Exams"] = df["Midterm 1"] + df["Midterm 2"]
+
+    hw_calc = lambda row: np.sum([row[f"Homework {i} (Total)"] for i in range(1, 10)])
+    df["Homework"] = df.apply(hw_calc)
+
+    df["Hog Project"] = (
+        df["Hog (Total)"] + df["Hog (Checkpoint 1)"] + df["Hog (Composition)"]
+    )
+    df["Cats Project"] = (
+        df["Cats (Total)"] + df["Cats (Checkpoint 1)"] + df["Cats (Composition)"]
+    )
+    df["Ants Project"] = (
+        df["Ants (Total)"] + df["Ants (Checkpoint 1)"] + df["Ants (Composition)"]
+    )
+
+    def scheme_calc(row):
+        scheme_raw = (
+            row["Scheme (Total)"]
+            + row["Scheme (Checkpoint 1)"]
+            + row["Scheme (Checkpoint 2)"]
+        )
+        return max(scheme_raw, row["Scheme Challenge Version (Total)"])
+
+    df["Scheme Project"] = df.apply(scheme_calc)
+    df["Projects"] = (
+        df["Hog Project"]
+        + df["Cats Project"]
+        + df["Ants Project"]
+        + df["Scheme Project"]
+    )
+
+    lab_calc = lambda row: np.sum(
+        [row[f"Lab {i} (Total)"] for i in range(1, 15) if i != 3]
+    )
+    df["Lab"] = df.apply(lab_calc)
+
+    dis_calc = lambda row: min(6, row["Tutorial Attendance (Raw)"])
+    df["Discussion"] = df.apply(dis_calc)
+
+    df["Adjustments"] = df["Hog Contest"] + df["Academic Dishonesty Penalty"]
+
+    df["Raw Score"] = (
+        df["Exams"]
+        + df["Homework"]
+        + df["Projects"]
+        + df["Lab"]
+        + df["Discussion"]
+        + df["Adjustments"]
+    )
+
+    upload = df.to_csv(index=False)
+
+    resp = make_response(upload)
+    resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    resp.headers["Content-Type"] = "text/csv"
+    return resp
 
 
 @job(app, "update_grades")
