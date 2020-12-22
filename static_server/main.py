97d1e6d4b72aba9b39fd0627c730d09e4c9e30fe
rahularya <rahularya@berkeley.edu>
2020-12-21 22:10:05 -0800

    Switch time to static_server

--- static_server/main.py
+++ static_server/main.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>", methods=["GET"])
 def get(path):
-    bucket = get_bucket({"static-server": "website", "website": "website"}, "website")
+    bucket = get_bucket(
+        {"static-server": "website", "website": "website", "time": "time"}, "website"
+    )
     return serve_path(bucket, "/", path)
 
 
