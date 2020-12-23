0ec61775487bd411708abf19d944386e065ced28
rahularya <rahularya@berkeley.edu>
2020-12-22 22:29:08 -0800

    Disable capped scores for howamidoing

--- howamidoing/public/config/elements.js
+++ howamidoing/public/config/elements.js
@@ -71,6 +71,8 @@ export function Topic(
     futureMaxScore = cappedScore;
   }
 
+  const uncapped = children.map((child) => child.uncapped).some();
+
   return {
     isTopic: true,
     name,
@@ -79,6 +81,7 @@ export function Topic(
     futureMaxScore,
     customCalculator,
     future,
+    uncapped,
   };
 }
 
@@ -125,4 +128,8 @@ export function LockedChildren(elem) {
 export function BooleanValued(elem) {
   elem.booleanValued = true;
   return elem;
+}
+
+export function Uncapped(elem) {
+  elem.uncapped = true;
 }
