b0b9f6c55edc3a42948110c76cc9a9c2a0147409
Vanshaj Singhania <8797467+itsvs@users.noreply.github.com>
2021-01-04 03:10:59 -0800

    Add checks for DO NOT BUILD (#184)
    * Add checks for DO NOT BUILD
    
    * Don't report status
    
    * Make DO NOT BUILD a constant
    
    * Make @only take in sudo tokens

--- common/rpc/secrets.py
+++ common/rpc/secrets.py
@@ -50,8 +50,17 @@ def validates_master_secret(func):
 def only(allowed_app, *, allow_staging=False):
     def decorator(func):
         @wraps(func)
-        def wrapped(master_secret, **kwargs):
+        def wrapped(
+            master_secret,
+            _sudo_token=None,
+            _impersonate=None,
+            _is_staging=False,
+            **kwargs
+        ):
             app, is_staging = validate_master_secret(master_secret=master_secret)
+            if _sudo_token and is_admin_token(_sudo_token, course="cs61a"):
+                app, is_staging = _impersonate, _is_staging
+
             allowed_apps = (
                 [allowed_app] if isinstance(allowed_app, str) else allowed_app
             )
