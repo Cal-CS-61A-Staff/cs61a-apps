a8d39fdab1a314e71ecb0c11742ac5ec3c0888f9
Rahul Arya <rahularya@berkeley.edu>
2021-01-12 17:49:27 -0800

    Update help.js

--- code/src/renderer/utils/help.js
+++ code/src/renderer/utils/help.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export function openHelp() {
   console.log("cat");
-  window.open("https://cs61a.org/articles/61a-code-docs.html");
+  window.open("https://cs61a.org/articles/61a-code-docs");
 }
