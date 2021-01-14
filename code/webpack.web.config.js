396969ac5faf8ea98987549cf5113b20a690b1f1
Rahul Arya <rahularya@berkeley.edu>
2021-01-14 09:38:55 -0800

    Add functionality to support embedding Code in an iframe (#201)

--- code/webpack.web.config.js
+++ code/webpack.web.config.js
@@ -79,7 +79,7 @@ module.exports = (env) => ({
       ELECTRON: false,
       SCHEME_COMPILE: (env && env.SCHEME_COMPILE) || false,
       __static: JSON.stringify("/static"),
-      VERSION: '"2.0.9"',
+      VERSION: '"2.0.10"',
     }),
     new MonacoWebpackPlugin({
       output: "./static",
