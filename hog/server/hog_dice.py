9bdf644524a6247f64a70bf88a89e338f09e7164
Pamela Fox <pamela.fox@gmail.com>
2021-01-21 16:49:31 -0800

    Remove custom dice code

--- hog/server/hog_dice.py
+++ hog/server/hog_dice.py
@@ -1,15 +1,4 @@
 from gui_files.svg import create_graphic, draw_rect, draw_circle, draw_line, draw_triangle, write_text
 
 def draw_dice(num):
-    x = 0
-    y = 0
-    width = 100
-    height = 100
-    graphic = create_graphic(width, height)
-    draw_rect(graphic, x, y, width, height, stroke="pink", fill="white")
-    spacing = width / (1 + num)
-    for _ in range(0, num):
-        x += spacing
-        y += spacing
-        draw_circle(graphic, x, y, 30)
-    return graphic
+   return None
