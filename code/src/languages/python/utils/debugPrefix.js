cae84055aff84b069b7bc34974db2ea27012a06a
rahularya <rahularya@berkeley.edu>
2021-02-01 00:11:04 -0800

    Hide test() from pytutor

--- code/src/languages/python/utils/debugPrefix.js
+++ code/src/languages/python/utils/debugPrefix.js
@@ -78,7 +78,7 @@ class Link:
         return string + str(self.first) + '>'
 
 
-#pythontutor_hide: draw, autodraw, disable_autodraw, visualize, editor, Tree, is_tree, tree, label, branches, Link
+#pythontutor_hide: draw, autodraw, disable_autodraw, visualize, editor, Tree, is_tree, tree, label, branches, Link, test
 
 # your code is below
 `;
