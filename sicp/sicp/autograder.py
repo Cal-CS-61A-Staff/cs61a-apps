2812c37912b91ae6c7a85140eaf0f6aac45fc63f
Vanshaj Singhania <svanshaj2001@gmail.com>
2021-02-06 22:27:44 -0800

    Add Autograder to SICP

---
+++ sicp/sicp/autograder.py
@@ -0,0 +1,39 @@
+import base64, requests, os
+from common.rpc.auth_utils import get_token, refresh_token, set_token_path
+from common.rpc.ag_master import upload_zip as upload, create_assignment as create
+
+
+class Autograder:
+    def __init__(self, course):
+        self.course = course
+
+        set_token_path(f"{os.path.expanduser('~')}/.sicp_token")
+        r = requests.get(
+            "https://okpy.org/api/v3/user/?access_token={}".format(get_token())
+        )
+        if not r.ok:
+            refresh_token()
+
+    def upload_zip(self, zip_file):
+        """
+        Uploads `filename` zip and returns the download url, overriding any
+        existing filesOverrides existing
+        """
+        assert zip_file.endswith(".zip"), "Upload Error"
+
+        with open(zip_file, "rb") as f:
+            upload(
+                token=get_token(),
+                course=self.course,
+                name=zip_file,
+                file=base64.b64encode(f.read()).decode("ascii"),
+            )
+
+    def create_assignment(self, assign, script, zip_file):
+        return create(
+            name=assign,
+            command=script,
+            file=zip_file,
+            course=self.course,
+            token=get_token(),
+        )
