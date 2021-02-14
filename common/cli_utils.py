b3dcf54dd115c99f05d8cd57c29a5cee810ca5fb
Vanshaj Singhania <svanshaj2001@gmail.com>
2021-02-14 08:59:17 -0800

    Initialize docs

--- common/cli_utils.py
+++ common/cli_utils.py
@@ -2,4 +2,9 @@ from colorama import Style
 
 
 def pretty_print(emoji, msg):
+    """Pretty prints a message.
+
+    :param emoji: the emoji to wrap the message in
+    :param msg: the message to wrap with the emoji
+    """
     print(f"{emoji}{Style.BRIGHT} {msg} {Style.RESET_ALL}{emoji}")
