92c355e6f79cf852c445e476c71f43ad5350f08d
Stephan Kaminsky <thaumicmekanism@gmail.com>
2021-03-05 20:35:19 -0800

    Moved out the rand_ids function

--- examtool/examtool/api/utils.py
+++ examtool/examtool/api/utils.py
@@ -22,22 +22,23 @@ def dict_to_list(d):
     return out
 
 
+def rand_id(length=32):
+    return "".join(random.choices(string.ascii_uppercase, k=length))
+
+
 def list_to_dict(l):
     return {i: x for i, x in enumerate(l)}
 
 
 class IDFactory:
     def __init__(self, id_start="", id_end="", length=32, allow_random_ids=True):
+        if length is None:
+            length = 32
         self.length = length
         self.id_start = id_start
         self.id_end = id_end
         self.allow_random_ids = allow_random_ids
         self.current_ids = set()
-
-    def rand_id(self, length=None):
-        if length is None:
-            length = self.length
-        return "".join(random.choices(string.ascii_uppercase, k=length))
 
     def id_from_str(self, string):
         return self.id_start + string + self.id_end
@@ -48,13 +49,13 @@ class IDFactory:
                 raise SyntaxError(
                     "A custom ID is required but was not set for this question!"
                 )
-            qid = self.rand_id()
+            qid = rand_id(length=self.length)
             while qid in self.current_ids:
-                qid = self.rand_id()
+                qid = rand_id(length=self.length)
         elif isinstance(string, str):
             qid = self.id_from_str(string)
             if qid in self.current_ids:
-                raise SyntaxError(f"Received duplicate question ID's {qid}.")
+                raise SyntaxError(f"Received duplicate question IDs {qid}.")
         else:
             raise SyntaxError("ID must be a string or None!")
 
