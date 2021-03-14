9d756a0f0e762ec970f2c671f7d6296b96251f6e
Rahul Arya <rahularya@berkeley.edu>
2021-03-14 01:56:23 -0800

    bump version

--- examtool/setup.py
+++ examtool/setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="examtool",
-    version="2.1.*",
+    version="2.2.*",
     author="Rahul Arya",
     author_email="rahularya@berkeley.edu",
     long_description=readme,
