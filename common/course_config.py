1d4a427900cee15bcd035f1d115334a79320af0f
Vanshaj Singhania <8797467+itsvs@users.noreply.github.com>
2021-04-02 15:59:42 -0700

    docs/init (#278)
    * Initialize docs
    
    * Add docs to static server
    
    * Rearrange conf.py
    
    * BWrite documentation docs
    
    * Add more common docs
    
    * Add common docs
    
    * Add branch instructions
    
    * fix things
    
    * Add typing hints
    
    * Add more typing hints
    
    * Add code segment hints

--- common/course_config.py
+++ common/course_config.py
@@ -11,6 +11,15 @@ ENDPOINT_ID = TTLCache(1000, 1800)
 
 
 def get_course(domain=None):
+    """Gets the course code of the course that owns the current domain, using
+    :meth:`~common.rpc.auth.get_domain`.
+
+    :param domain: the domain name in question, inferred using
+        :meth:`~common.course_config.get_domain` if omitted
+    :type domain: str
+
+    :return: the course code, such as "cs61a"
+    """
     if getenv("ENV") != "prod":
         return "cs61a"
     if not domain:
@@ -23,10 +32,23 @@ def get_course(domain=None):
 
 
 def get_domain():
+    """Gets the domain that this request is being made on.
+
+    :return: the domain name, based on request headers
+    """
     return request.headers.get("X-Forwarded-For-Host") or request.headers["HOST"]
 
 
 def get_endpoint(course=None):
+    """Gets a course's most recent Okpy endpoint, typically the current
+    semester's, using :meth:`~common.rpc.auth.get_endpoint`.
+
+    :param course: the course code, such as "cs61a", inferred using
+        :meth:`~common.course_config.get_course` if omitted
+    :type course: str
+
+    :return: the Okpy endpoint, such as "cal/cs61a/sp21"
+    """
     if getenv("ENV") != "prod":
         return "cal/cs61a/sp21"
     if not course:
@@ -37,6 +59,15 @@ def get_endpoint(course=None):
 
 
 def get_course_id(course=None):
+    """Gets a course's most recent Okpy course ID, typically the current
+    semester's, using :meth:`~common.rpc.auth.get_endpoint_id`.
+
+    :param course: the course code, such as "cs61a", inferred using
+        :meth:`~common.course_config.get_course` if omitted
+    :type course: str
+
+    :return: the Okpy endpoint, such as 707
+    """
     if getenv("ENV") != "prod":
         return 1
     if not course:
@@ -47,6 +78,17 @@ def get_course_id(course=None):
 
 
 def is_admin(email, course=None):
+    """Returns whether or not an email address belongs to an admin
+    for the given course, using :meth:`~common.rpc.auth.is_admin`.
+
+    :param email: the email address in question
+    :type email: str
+    :param course: the course code, such as "cs61a", inferred using
+        :meth:`~common.course_config.get_course` if omitted
+    :type course: str
+
+    :return: ``True`` if the user is an admin, ``False`` otherwise
+    """
     if getenv("ENV") != "prod":
         return True
     if not course:
@@ -55,6 +97,17 @@ def is_admin(email, course=None):
 
 
 def is_admin_token(access_token, course=None):
+    """Returns whether or not an Okpy access token belongs to an admin
+    for the given course.
+
+    :param access_token: the Okpy access token in question
+    :type access_token: str
+    :param course: the course code, such as "cs61a", inferred using
+        :meth:`~common.course_config.get_course` if omitted
+    :type course: str
+
+    :return: ``True`` if the user is an admin, ``False`` otherwise
+    """
     ret = requests.get(
         "https://okpy.org/api/v3/user/", params={"access_token": access_token}
     )
@@ -65,5 +118,13 @@ def is_admin_token(access_token, course=None):
 
 
 def format_coursecode(course):
+    """Formats a course code is a pretty way, separating the department from
+    the course number.
+
+    :param course: the course code, such as "cs61a"
+    :type course: str
+
+    :return: prettified course code, such as "CS 61A"
+    """
     m = re.match(r"([a-z]+)([0-9]+[a-z]?)", course)
     return m and (m.group(1) + " " + m.group(2)).upper()
