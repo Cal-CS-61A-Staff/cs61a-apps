93c0cd22d305c009caea12dcfd1e2c0c84b53e5b
Rahul Arya <rahularya@berkeley.edu>
2021-04-17 21:38:17 -0700

    Shared buildtool cache (#366)

--- sandbox/ide.py
+++ sandbox/ide.py
@@ -161,6 +161,9 @@ def start():
     sh("chown", "-R", username, f"/save/{username}")
     print("Home folder owner set.", file=sys.stderr)
 
+    sh("mkdir", "-p", "/save/.cache")
+    sh("chmod", "a+rw", "/save/.cache")
+
     if not get_server_pid(username):
         print(f"Server for {username} is not running, starting...", file=sys.stderr)
         with db_lock("ide", username):
