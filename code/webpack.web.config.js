e47399e38f4fec24803774afbf9b914a15e09347
Rahul Arya <rahularya@berkeley.edu>
2021-05-06 14:09:49 -0700

    Fix multiline input in python terminal

--- code/webpack.web.config.js
+++ code/webpack.web.config.js
@@ -79,7 +79,7 @@ module.exports = (env) => ({
       ELECTRON: false,
       SCHEME_COMPILE: (env && env.SCHEME_COMPILE) || false,
       __static: JSON.stringify("/static"),
-      VERSION: '"2.6.1"',
+      VERSION: '"2.6.2"',
     }),
     new webpack.ProvidePlugin({
       $: "jquery",
